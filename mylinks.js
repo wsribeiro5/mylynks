f497e8ea96921094a2f223c84d4fb266094ad0ec
Wellyngton S. Ribeiro <wsribeiro5@hotmail.com>
2020-07-25 15:50:23 -0300

    Version Free 1.0

---
+++ mylinks.js
@@ -0,0 +1,58 @@
+mylinks = { 
+    titulo: {
+      codigo: 13021986,
+      myGit: '',
+      linkDaSuaFoto: '',//Coloque aqui o link da sua foto ou da sua empresa no insta ou facebook
+      seuNome: 'Seu nome ou do seu negócio',
+      tamanhoTexto: 30,
+      corTexto: ''
+    },
+    fundo: {
+      cor: "Lavender"
+    },
+    botoes:
+      [        
+      {
+          textoDoBotao: "Coloque aqui o nome do seu link", 
+          linkDoBotao: "Coloque aqui o seu link",
+          corDoBotao: "white",
+          corDaLetra: "", 
+          tamanhoDaLetra: '20', 
+          dataInicio: '', 
+          dataFim: ""
+        },// copie e cole sempre com a virgula no final para criar um novo botão
+        {
+          textoDoBotao: "Coloque aqui o nome do seu outro link", 
+          linkDoBotao: "Coloque aqui o seu outro link",
+          corDoBotao: "black",
+          corDaLetra: "white", 
+          tamanhoDaLetra: '30', 
+          dataInicio: '', 
+          dataFim: ""
+        },// copie e cole sempre com a virgula no final para criar um novo botão        
+      ],
+    redesSociais:
+      [
+        {
+          nome: 'facebook',
+          link: 'Coloque aqui o link do seu facebook',
+          imagem: 'https://wsribeiro5.github.io/wellyngtonsampaio.github.io/facebook.png'
+        },
+        {
+          nome: 'instagram',
+          link: 'Coloque aqui o link do seu instagram',
+          imagem: 'https://wsribeiro5.github.io/wellyngtonsampaio.github.io/instagram.png'
+        },
+        {
+          nome: 'youtube',
+          link: 'Coloque aqui o link do seu youtube',
+          imagem: 'https://wsribeiro5.github.io/wellyngtonsampaio.github.io/youtube.png'
+        },
+        {
+          nome: 'twitter',
+          link: 'Coloque aqui o link do seu twitter',
+          imagem: 'https://wsribeiro5.github.io/wellyngtonsampaio.github.io/twitter.png'
+        },// copie e cole sempre com a virgula no final para criar uma nova rede social
+      ]
+  }
+  //FIM DOS DADOS
